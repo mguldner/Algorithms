8604bba25452e6a6efb3c5589d3d21bfe8cdd276
mguldner <mathieuguldner.emn@gmail.com>
2017-04-05 14:47:29 +0200

    Add Implementation of a VertexComparator

---
+++ Java/Graph Theory/VertexComparator.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class VertexComparator implements Comparator<Vertex> {
+    
+    public int compare(Vertex o1, Vertex o2){
+        if(o1.getDist() == -1)
+            return 1;
+        else if(o2.getDist() == -1)
+            return -1;
+        else{
+            if(o1.getDist() < o2.getDist())
+                return -1;
+            else if(o1.getDist() > o2.getDist())
+                return 1;
+            else
+                return 0;
+        } 
+    }
+}
