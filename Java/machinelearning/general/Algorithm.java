2f99a47aa0a93006e1609227a4765b1e9a0794cc
mguldner <mathieuguldner.emn@gmail.com>
2017-04-18 17:03:27 +0200

    Update Algorithm.java

--- Java/machinelearning/general/Algorithm.java
+++ Java/machinelearning/general/Algorithm.java
@@ -1,18 +1,17 @@
-package machinelearning.general;
+package general;
 
 import java.util.List;
 
-import machinelearning.decisiontree.Feature;
-import machinelearning.decisiontree.Feature.FeatureType;
+import decisiontree.Feature;
 
 /*
  * T : type de retour de l'aglorithme
  * U : type d'input de l'algorithme
  */
 
-public interface Algorithm<T,U extends DataObject> {
+public interface Algorithm<U extends DataObject, T> {
 
-	T train(List<U> data, List<Feature<FeatureType>> features);
+	T train(List<U> data, List<Feature<?>> features);
 	
 	T test();
 }
