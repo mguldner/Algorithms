3363369daa317f853cd79eab78c22c5c195012b5
mguldner <mathieuguldner.emn@gmail.com>
2017-04-18 17:02:46 +0200

    Update RunExample.java

--- Java/machinelearning/example/titanic/RunExample.java
+++ Java/machinelearning/example/titanic/RunExample.java
@@ -1,4 +1,4 @@
-package machinelearning.example.titanic;
+package example.titanic;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import machinelearning.decisiontree.Feature;
-import machinelearning.decisiontree.Feature.FeatureType;
-import machinelearning.decisiontree.data.Node;
-import machinelearning.decisiontree.data.Tree;
-import machinelearning.general.LossFunction;
+import decisiontree.Feature;
+import decisiontree.Feature.FeatureType;
+import decisiontree.data.Node;
+import decisiontree.data.Tree;
+import example.titanic.features.Frame;
+import general.LossFunction;
 
 public class RunExample {
 
@@ -62,11 +63,26 @@ public class RunExample {
 						values[10]));
 			}
 		}
-		// Liste a definir auparavant et a completer au fur et a mesure de la lecture des donnees d'entree
+		// Listes a definir auparavant et a completer au fur et a mesure de la lecture des donnees d'entree
 		ArrayList<String> possibleEmbarcations = new ArrayList<String>();
-		possibleEmbarcations.add("Test");
-		Feature<String> embarcation = new Feature<String>("Embarcation", possibleEmbarcations);
-		List<Feature<FeatureType>> features = new ArrayList<Feature<FeatureType>>();
+		possibleEmbarcations.add("C");
+		possibleEmbarcations.add("Q");
+		possibleEmbarcations.add("S");
+		Feature<String> embarcation = new Feature<String>("embarked", possibleEmbarcations);
+		
+		ArrayList<Integer> possiblePClasses = new ArrayList<Integer>();
+		possiblePClasses.add(1);
+		possiblePClasses.add(2);
+		possiblePClasses.add(3);
+		Feature<Integer> passengerClass = new Feature<Integer>("pClass", possiblePClasses);
+		
+		ArrayList<Frame> possibleAges = new ArrayList<Frame>();
+		possibleAges.add(new Frame(0,18));
+		possibleAges.add(new Frame(18,50));
+		possibleAges.add(new Frame(50,100));
+		Feature<Frame> age = new Feature<Frame>("age", possibleAges);
+		
+		List<Feature<?>> features = new ArrayList<Feature<?>>();
 		features.add(embarcation);
 		Tree<Answer> tree = new Node<Feature>();
 		tree.setTrainingSet();
