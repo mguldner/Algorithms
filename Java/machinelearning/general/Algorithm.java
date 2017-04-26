5bf209ce449b7547e2291bbe464b8192c0dad35d
mguldner <mathieuguldner.emn@gmail.com>
2017-04-26 14:28:48 +0200

    Update Algorithm.java

--- Java/machinelearning/general/Algorithm.java
+++ Java/machinelearning/general/Algorithm.java
@@ -2,16 +2,19 @@ package general;
 
 import java.util.List;
 
+import decisiontree.Answer;
 import decisiontree.Feature;
+import decisiontree.data.Tree;
 
 /*
- * T : type de retour de l'aglorithme
- * U : type d'input de l'algorithme
+ * V : type de retour de l'aglorithme -> Un arbre dans le cas d'un algorithme d'arbre de decision
+ * T : type d'input de l'algorithme -> le type de la donnée d'entrée : un Passenger<Integer> pour le titanic
+ * U : type de la valeur final predite par l'algorithme -> Integer dans le cas du titanic (mort(0) ou vivant(1)).
  */
 
-public interface Algorithm<U extends DataObject, T> {
+public interface Algorithm<U, T extends DataObject<U>, V> {
 
-	T train(List<U> data, List<Feature<?>> features);
+	V train(List<T> data, List<Feature<?>> features);
 	
-	T test();
+	public U test(Tree<Answer<?,U>> tree, T testPoint);
 }
