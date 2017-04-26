3ba0f25703e1269bffad8bcc45fa175ccc471e38
mguldner <mathieuguldner.emn@gmail.com>
2017-04-26 14:29:00 +0200

    Update DataObject.java

--- Java/machinelearning/general/DataObject.java
+++ Java/machinelearning/general/DataObject.java
@@ -1,10 +1,18 @@
 package general;
 
+import java.util.List;
+
 import decisiontree.Feature;
 
-public interface DataObject {
+/**
+ * @param <U> : Le type de la valeur finale (la donnee que l'on cherche a prevoir). 
+ * Dans le cas du titanic, un Integer 0 (mort) ou 1 (vivant).
+ */
+public interface DataObject<U> {
 
-	public String getLabel();
+	public U getLabel();
 	
 	public int classificationForFeature(Feature<?> feature);
+	
+	public int[][] getOverview(List<Feature<?>> features);
 }
